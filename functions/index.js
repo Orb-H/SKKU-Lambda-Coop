af6d20c06c7fc3bca565b3000a6600c97539fc70
Orb_H <orb.cov@gmail.com>
2019-07-05 13:50:17 +0900

    index.js recover

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.login = functions.https.onRequest((request, response) => {
+  var id = request.body.id;
+  var pw = request.body.pw;
+
+  if (id === 'admin' && pw === 'admin321') {
+    response.send('S');
+  } else if (id !== 'admin') {
+    response.send('I');
+  } else if (pw !== 'admin321') {
+    response.send('P');
+  }
+  response.send('?');
+});
