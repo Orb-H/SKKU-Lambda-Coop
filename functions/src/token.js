6613f06d38aaae417c6bb84a797beb4b3857d697
Orb_H <orb.cov@gmail.com>
2019-07-17 11:52:41 +0900

    token.js function name edit

--- functions/src/token.js
+++ functions/src/token.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 module.exports = {
-  sendtoken: functions.https.onRequest((req, res) => {
+  sendToken: functions.https.onRequest((req, res) => {
 
   }),
   getUserToken: functions.https.onRequest((req, res) => {
