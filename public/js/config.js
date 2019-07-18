ed2c84f12f7683addcc8722ee6a97ed88ca767a2
Orb_H <orb.cov@gmail.com>
2019-07-18 14:07:32 +0900

    public/user.html full implementation

---
+++ public/js/config.js
@@ -0,0 +1,19 @@
+const config = {
+  chainId: '5300575914426995782',
+  mt: {
+    symbol: 'FT9754',
+  },
+  st: {
+    symbol: 'SKK',
+  },
+  walletAddress: {
+    system: '0x04a4103cb990ecc28c6dd882b08a64f1bdb6ffc2',
+  },
+  dapp: {
+    apiKey: '9d6mUgGSFEE2BesmdLUWdC75aL4mtFrW2spEVGWRNrY33oXrB4wwgcccMAA8F8Xx'
+  },
+  txActionName: {
+    adminSend: "adminSend"
+  },
+  userName: 'OrbCov',
+}
