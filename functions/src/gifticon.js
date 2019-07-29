455e530b1098a356c67743f0f63f36bd0aa9910d
cseho67 <48603413+cseho67@users.noreply.github.com>
2019-07-29 14:08:59 +0900

    client.js 수정

--- functions/src/gifticon.js
+++ functions/src/gifticon.js
@@ -4,7 +4,6 @@ const {
   db,
 } = require('./admin.js');
 const login = require('./login.js');
-
 module.exports = {
   //2. 웹->서버 기프티콘 전달 요청주소 POST/gifticons/register
   webtoservergift: functions.https.onRequest(async(req, res) => {
@@ -48,10 +47,7 @@ module.exports = {
       res.status(404).send('');
     }
   }),
-
-
   //3. 서버->웹 기프티콘 리스트 전달 요청주소 POST/gifticons/list
-  // name , category1, category2, price ,count json 형식으로 보내죽 ㅣ
   servertowebgift: functions.https.onRequest(async (req, res) => {
     if (req.method === 'POST') {
       var body = req.body;
@@ -100,62 +96,6 @@ module.exports = {
       res.status(404).send('');
     }
   }),
-  
-  /*servertowebgift: functions.https.onRequest(async (req, res) => {
-    if (req.method === 'POST') {
-      var body = req.body;
-      var isadmin = await login.checkadmin(body.token);
-      var obj = {
-        "result": "false",
-        "data": {}
-      }
-      if (!isadmin) {
-        res.send(401).send(JSON.stringify(obj));
-      }
-
-      obj.result = "true";
-      obj.data.content = [];
-
-      try {
-        await db.collection('gifticon').get().then((snapshot) => {
-          snapshot.forEach(async (doc) => {
-            var temp = 0;
-            var temp2 = 1;
-            var data = doc.data();
-            for (var i = 0; i < obj.data.content.length; i++) {
-              if (obj.data.content[i].name === data.name && obj.content[i].category1 === data.category1 && obj.content[i].category2 === data.category2) {
-                temp = 1;
-                temp2 = i;
-                break;
-              }
-            }
-            if (temp === 1) {
-              obj.data.content[i].count++;
-            } else if (temp === 0) {
-              obj.data.content.push({
-                name: data.name,
-                category1: data.category1,
-                category2: data.category2,
-                cost: data.price,
-                count: 1
-              });
-            }
-            return obj;
-          });
-
-          obj.data.length = obj.data.content.length;
-          res.send(JSON.stringify(obj));
-
-          return snapshot;
-        }).catch((err) => {
-          res.status(500).send(err.message);
-        });
-      } catch (err) {
-        res.status(500).send(err.message);
-      }
-    }
-  }),
-  */
   //4. 기프티콘 특정 Type 정보 전달 요청주소 POST/gifticons/detail
   gtype: functions.https.onRequest(async(req, res) => {
       if (req.method === 'POST') {
@@ -194,37 +134,6 @@ module.exports = {
         res.status(404).send('');
       }
     }),
-       /*   
-          var dbquery =  db.collection('gifticon')
-          .where('name', '==', data.name)
-          .where('category1', '==', data.category1)
-          .where('category2', '==', data.category2)
-          .get().then((snapshot) => {
-            snapshot.forEach(async(doc) =>{
-              let encodedimage = doc.data().image.toString('base64');
-                    obj.data.content.push({
-                      id: doc.id,
-                      image: encodedimage,
-                      used: doc.data().used
-                    });
-              return obj;
-            });
-            obj.data.length = obj.data.content.length;
-            res.send(JSON.stringify(obj));
-            return snapshot;
-          }).catch((err) => {
-            res.status(500).send(err.message);
-          });
-        }catch(err){
-          res.status(500).send(err.message);
-        }
-      }
-      else{
-        res.status(404).send('');
-      }
-    }),
-*/
-
   //5. 기프티콘 삭제 요청 요청주소 POST/gifticons/remove
   gdelete: functions.https.onRequest(async(req, res) => {
     if (req.method === 'POST') {
@@ -235,7 +144,9 @@ module.exports = {
         "data": {}
       }
       if (!isadmin) {
+
         res.send(401).send(JSON.stringify(obj));
+      
       }
       obj.result = "true";
       try{
@@ -253,21 +164,3 @@ module.exports = {
     }
   })
 };
-
-
-
-        /*
-        var did = body.id;
-        await db.collection('gifticons').doc("did").get().then((snapshot)=>{
-          snapshot.forEach(async(doc)=>{
-
-            await db.collection('gifticon').doc('did').delete();
-            obj.data.push({
-               success: "true"
-            });
-          })
-        }).catch((err) => {
-          res.status(500).send(err.message);
-        });
-        */
-      
